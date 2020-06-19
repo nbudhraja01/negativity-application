71c791eb2786ece9a3067d78fe8f03192978941a
NAVI <nbudhraj@ucsd.edu>
2020-06-18 17:12:44 -0700

    Initial Commit

---
+++ MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Navi Budhraja on 6/18/20.
+//  Copyright © 2020 Navi Budhraja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
